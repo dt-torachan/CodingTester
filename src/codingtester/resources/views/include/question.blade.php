3c7b8fd7e10b222e753ddcd22c01376b48ca25b0
dt-torachan <55536520+dt-torachan@users.noreply.github.com>
2022-03-14 20:01:19 +0900

    リンク及びサムネイルの追加

--- src/codingtester/resources/views/include/question.blade.php
+++ src/codingtester/resources/views/include/question.blade.php
@@ -1,11 +1,13 @@
 <div class="question_blade">
-    <div class="question_blade_img">
-        <img src="noimg.png" alt="">
-    </div>
-    <div class="question_blade_title">
-        {{$question->title}}
-    </div>
-    <div class="question_blade_lead">
-        {{$question->lead}}
-    </div>
+    <a href="/question/detail/{{$question->question_id}}" style="display: block;">
+        <div class="question_blade_img">
+            <img src="/photo/{{$question->photo}}" alt="">
+        </div>
+        <div class="question_blade_title">
+            {{$question->title}}
+        </div>
+        <div class="question_blade_lead">
+            {{$question->lead}}
+        </div>
+    </a>
 </div>
