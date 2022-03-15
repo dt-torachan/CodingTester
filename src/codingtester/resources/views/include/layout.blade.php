e91d0dd1cb75139e723791b1a3a17b820c1dbb0b
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-03-15 14:25:56 +0900

    不要なbladeを除去

--- src/codingtester/resources/views/include/layout.blade.php
+++ src/codingtester/resources/views/include/layout.blade.php
@@ -1,5 +1,5 @@
 <link href="{{ asset('css/common.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('css/questions.blade.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('css/header.blade.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/questions.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css">
 @include("include.header")
 @include('include.footer')
