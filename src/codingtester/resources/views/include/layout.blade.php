fba2207601098e53971e88e914f987272087e8c5
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-03-17 02:29:18 +0900

    問題詳細ページのCSSを作成しました。

--- src/codingtester/resources/views/include/layout.blade.php
+++ src/codingtester/resources/views/include/layout.blade.php
@@ -1,6 +1,8 @@
 <link href="{{ asset('css/common.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/questions.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/answer.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/detail.css') }}" rel="stylesheet" type="text/css">
 
 <script src="{{ asset('/js/jquery-3.6.0.min.js') }}"></script>
 
