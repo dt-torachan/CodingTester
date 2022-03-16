5fc552197a08734b15e129cf56c55e9f88dbbd6f
alj_katagiri <katagiri@al-j.co.jp>
2022-03-16 22:11:50 +0900

    jQueryの導入

--- src/codingtester/resources/views/include/layout.blade.php
+++ src/codingtester/resources/views/include/layout.blade.php
@@ -1,5 +1,6 @@
 <link href="{{ asset('css/common.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/questions.blade.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/header.blade.css') }}" rel="stylesheet" type="text/css">
+<script src="{{ asset('/js/jquery-3.6.0.min.js') }}"></script>
 @include("include.header")
 @include('include.footer')
