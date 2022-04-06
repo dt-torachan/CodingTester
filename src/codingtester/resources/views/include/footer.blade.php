79132a5a571fed49ab9fc37d4955adde96f15527
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-04-06 21:04:49 +0900

    フッターのHTMLを編集しました

--- src/codingtester/resources/views/include/footer.blade.php
+++ src/codingtester/resources/views/include/footer.blade.php
@@ -1,3 +1,11 @@
 <footer>
+    <div class="logo m-b-md">
+        <img src="/photo/logo.png" alt="logo">
+    </div>
+    <ul class="sns">
+        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li><a href="https://github.com/dt-torachan/sandbox"><i class="fa-brands fa-github"></i></a></li>
+    </ul>
+    <small>©2022 - CodingTester  |   All right reserved</small>
 
 </footer>
