a8c551d25b9c5a7389374657c7e8653d93e2031a
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-04-06 20:51:33 +0900

    fontawesomeを使用できるようにしました

--- src/codingtester/resources/views/include/layout.blade.php
+++ src/codingtester/resources/views/include/layout.blade.php
@@ -4,6 +4,7 @@
 <link href="{{ asset('css/answer.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/detail.css') }}" rel="stylesheet" type="text/css">
 
+<link href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" rel="stylesheet">
 <script src="{{ asset('/js/jquery-3.6.0.min.js') }}"></script>
 
 @include("include.header")
