97e83188c969bed60d1bb77d3f399d06fe008a53
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-04-06 20:52:02 +0900

    ヘッダーを編集しました

--- src/codingtester/resources/views/include/header.blade.php
+++ src/codingtester/resources/views/include/header.blade.php
@@ -1,15 +1,17 @@
 <header>
     <div class="content">
-        <div class="title m-b-md">
-            CodingTester
+        
+        <div class="logo m-b-md">
+            <img src="/photo/logo.png" alt="logo">
+            <h1>CodingTester</h1>
         </div>
 
         <div class="links">
             <a href="/">TOP</a>
-            <a href="/question?new=1">Recentry</a>
             <a href="/question">Questions</a>
             <a href="/news">News</a>
             <a href="https://github.com/dt-torachan/sandbox">GitHub</a>
         </div>
+        <div><a href="/mypage"><i class="fa-solid fa-user"></i></a></div>
     </div>
 </header>
