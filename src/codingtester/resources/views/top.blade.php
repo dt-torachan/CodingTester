5906996800f368147f5bb42eaa2a39eed42114ca
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-04-09 11:16:46 +0900

    トップページのヒーロー部分のHTMLを作成しました。

--- src/codingtester/resources/views/top.blade.php
+++ src/codingtester/resources/views/top.blade.php
@@ -9,10 +9,30 @@
 {{-- content --}}
 @section('content')
 <section class="hero">
-hero
+    <div class="hero_text">
+        <p>Welcome to</br>
+        CodingTester</p>
+        <div>
+            <p>プログラミングの基礎が学べる</p>
+            <p>プログラミングの実力を試せる</p>
+            <p>プログラミングの</p>
+        </div>
+    </div>
+    <div class="form_registration">
+        <p>今すぐ登録して</br>
+        実力を試そう！</p>
+        <div>
+            <form action="">
+                <input type="text" placeholder="UserID">
+                <input type="mail" placeholder="Email">
+                <input type="password" placeholder="PassWord">
+            </form>
+            <button>Start now!</button>
+        </div>
+    </div>
 </section>
 <section class="questions">
-questions
+    
 </section>
 <section class="news">
 news
