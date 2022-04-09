e049419f6da94bf98ff90544e7fd0d2c57530c8c
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2022-04-09 20:56:33 +0900

    ヒーロー部分のCSSを作成し、中央に来るようにしました。

--- src/codingtester/resources/views/top.blade.php
+++ src/codingtester/resources/views/top.blade.php
@@ -4,74 +4,87 @@
 @section('keywords', 'キーワード1,キーワード2,キーワード3')
 @section('description', '説明文です')
 
+{{-- CSS --}}
+@section('pageCss')
+    <link href="{{ asset('css/top.css') }}" rel="stylesheet" type="text/css">
+@endsection
+
 @include('include.header')
  
 {{-- content --}}
 @section('content')
 <section class="hero">
-    <div class="hero_text">
-        <p>Welcome to</br>
-        CodingTester</p>
-        <div>
-            <p>プログラミングの基礎が学べる</p>
-            <p>プログラミングの実力を試せる</p>
-            <p>プログラミングの</p>
+    <div class="section_wrap">
+        <div class="hero_content">
+            <p class="hero_title">Welcome to</br>
+            CodingTester</p>
+            <div class="hero_text">
+                <p>プログラミングの基礎が学べる</p>
+                <p>プログラミングの実力を試せる</p>
+                <p>プログラミングの</p>
+            </div>
         </div>
-    </div>
-    <div class="form_registration">
-        <p>今すぐ登録して</br>
-        実力を試そう！</p>
-        <div>
-            <form action="">
-                <input type="text" placeholder="UserID">
-                <input type="mail" placeholder="Email">
-                <input type="password" placeholder="PassWord">
-            </form>
-            <button>Start now!</button>
+        <div class="registration">
+            <p class="registration_text">今すぐ登録して</br>
+            出題しよう！</p>
+            <div class="registration_form">
+                <form action="">
+                    <input type="text" placeholder="UserID">
+                    <input type="mail" placeholder="Email">
+                    <input type="password" placeholder="PassWord">
+                </form>
+                <button class="btn">Start now!</button>
+            </div>
         </div>
     </div>
 </section>
 <section class="questions">
-    <h2>QUESTIONS</h2>
-    <p>挑戦したい問題を見つけよう！</p>
-    <div class="questions_menu">
-    </div>
-    <div class="questions_wrap">
-        <div class="question">
-            <img src="photo/logo.png" alt="">
-            <div class="question_text">
-                <p class="question_title">FizzBuzz</p>
-                <p class="question_description">プログラミングの基礎！好きな言語で挑戦しよう</p>
-                <div class="question_btn">問題を解く</div>
+    <div class="section_wrap">
+        <h2>QUESTIONS</h2>
+        <p>挑戦したい問題を見つけよう！</p>
+        <div class="questions_menu">
+        </div>
+        <div class="questions_wrap">
+            <div class="question">
+                <img src="photo/logo.png" alt="">
+                <div class="question_text">
+                    <p class="question_title">FizzBuzz</p>
+                    <p class="question_description">プログラミングの基礎！好きな言語で挑戦しよう</p>
+                    <div class="question_btn btn">問題を解く</div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="btn">
-        <p>READ ALL</p>
+        <div class="btn">
+            <p>READ ALL</p>
+        </div>
     </div>
 </section>
 <section class="news">
-    <h2>NEWS</h2>
-    <div class="news_wrap">
-        <div class="news_block">
-            <p>問題を追加しました</p>
-            <div class="new_btn"></div>
+    <div class="section_wrap">
+        <h2>NEWS</h2>
+        <div class="news_wrap">
+            <div class="news_block">
+                <p>問題を追加しました</p>
+                <div class="new_btn"></div>
+            </div>
         </div>
-    </div>
-    <div class="btn">
-        <p>READ ALL</p>
+        <div class="btn">
+            <p>READ ALL</p>
+        </div>
     </div>
 </section>
 <section class="faq">
-    <h2>FAQ</h2>
-    <div class="faq_wrap">
-        <div class="faq_block">
-            <p>回答できる言語は選択できますか？</p>
-            <p>選択できます。Python、、、などの言語を選択して回答いただけます。</p>
+    <div class="section_wrap">
+        <h2>FAQ</h2>
+        <div class="faq_wrap">
+            <div class="faq_block">
+                <p>回答できる言語は選択できますか？</p>
+                <p>選択できます。Python、、、などの言語を選択して回答いただけます。</p>
+            </div>
         </div>
-    </div>
-    <div class="btn">
-        <p>READ ALL</p>
+        <div class="btn">
+            <p>READ ALL</p>
+        </div>
     </div>
 </section>
 
